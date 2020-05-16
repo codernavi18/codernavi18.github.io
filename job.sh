33be95d691a70c926d3ea73151af3f3ba18cda2c
codernavi18 <codernavi18@gmail.com>
2020-05-17 02:02:09 +0530

    Sun May 17 02:02:09 IST 2020

--- job.sh
+++ job.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
-#clone the repository
-echo "Hello World"
-pwd
+#enter the scrapy directory
 pushd rankings
+
+#run the spiders
 scrapy crawl codechef -t json -o codechef.json.tmp
-ls
+scrapy crawl codeforces -t json -o codeforces.json.tmp
+scrapy crawl leetcode -t json -o leetcode.json.tmp
+scrapy crawl atcoder -t json -o atcoder.json.tmp
+scrapy crawl uva -t json -o uva.json.tmp
+
+#replace the new files (if any) with the older ones
+mv codechef.json.tmp codechef.json
+mv codeforces.json.tmp codeforces.json
+mv leetcode.json.tmp leetcode.json
+mv atcoder.json.tmp atcoder.json
+mv uva.json.tmp uva.json
+
+git config user.email "travis@travis-ci.org"
+git config user.name "Travis CI"
+git add .
+git commit -m "Travis build : $TRAVIS_BUILD_NUMBER [$(date)]"
+git remote add origin-pages https://${GH_TOKEN}@github.com/codernavi18/codernavi18.github.io.git > /dev/null 2>&1
+git push --quiet --set-upstream origin-pages master
+exit 0
