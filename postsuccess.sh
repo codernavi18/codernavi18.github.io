20e08db5047a77e7235ea167a51aaac70e5e2c9d
codernavi18 <codernavi18@gmail.com>
2020-05-17 02:49:10 +0530

    Sun May 17 02:49:10 IST 2020

--- postsuccess.sh
+++ postsuccess.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 git status
+echo "github token is this : $GITHUB_TOKEN"
 
 git add .
 git commit -m "Travis build : $TRAVIS_BUILD_NUMBER [$(date)]"
 git remote rm origin
-git remote add origin https://codernavi18:ccf1957fea6882e10758ba32113232d4252bc5db@github.com/codernavi18/codernavi18.github.io.git > /dev/null 2>&1
+git remote add origin https://codernavi18:$GITHUB_TOKEN@github.com/codernavi18/codernavi18.github.io.git > /dev/null 2>&1
 git push origin master --quiet
 exit 0
