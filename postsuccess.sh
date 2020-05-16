16e19faa76acb22630eabb76db44fe3c77b9cda9
codernavi18 <codernavi18@gmail.com>
2020-05-17 02:41:10 +0530

    Sun May 17 02:41:10 IST 2020

--- postsuccess.sh
+++ postsuccess.sh
@@ -4,6 +4,6 @@ git status
 git add .
 git commit -m "Travis build : $TRAVIS_BUILD_NUMBER [$(date)]"
 git remote rm origin
-git remote add origin https://codernavi18:668e42c4779974bb4141f03e9118647fba0d098d@github.com/codernavi18/codernavi18.github.io.git > /dev/null 2>&1
+git remote add origin https://codernavi18:ccf1957fea6882e10758ba32113232d4252bc5db@github.com/codernavi18/codernavi18.github.io.git > /dev/null 2>&1
 git push origin master --quiet
 exit 0
